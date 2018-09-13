06e8d02ab1cce936ad9887f923afdb26dec17b24
kamilaribeiro <42790156+kamilaribeiro@users.noreply.github.com>
2018-09-12 22:46:11 -0300

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int estado = 0, i = 0;
+    char bin[256];
+    printf("\nDigite um numero em binario");
+    scanf("%s", bin);
+    while(bin[i] != '\0')
+    {
+    if(bin[i] == '0')
+       {
+           if(estado == 0);
+            else if(estado == 1) estado = 2;
+            else if(estado == 2) estado = 1;
+            i++;
+        }
+        else if(bin[i] == '1')
+        {
+            if(estado == 0) estado = 1;
+            else if(estado == 1) estado = 0;
+            else if(estado == 2);
+            i++;
+        }
+        else
+        {
+            printf("\nNumero digitado nao e um numero binario");
+            return 0;
+        }
+    }
+    if(estado == 0) printf("\n O numero digitado %s e multiplo de 3", bin);
+    else printf("\n O numero binario digitado %s nao e multiplo de 3", bin);
+    return 0;
+} 
